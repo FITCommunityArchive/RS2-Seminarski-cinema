fecff5ca302e398a05c4a0beada29c67f3784499
unknown <boris.h@aquila-ars.com>
2019-11-17 21:54:03 +0100

    Minor fix for the RelativePath, removing obsolete functions

--- Cinema.Seed/Program.cs
+++ Cinema.Seed/Program.cs
@@ -7,15 +7,23 @@ namespace Cinema.Seed
 {
     class Program
     {
+
+        public static string path = "\\LegacyDatabase\\CinemaDatabase.xlsx";
+        public static string workingDirectory = Environment.CurrentDirectory;
+        public static string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        public static string fullPath = projectDirectory + path;
+
         static void Main(string[] args)
         {
-            FileInfo file = new FileInfo(@"C:\Users\Lazarus\FIT\webapps\webapp\Cinema.Seed\LegacyDatabase\CinemaDatabase.xlsx");
+            
+            FileInfo file = new FileInfo(fullPath);
+
             string connectionString = "Server=(localdb)\\mssqllocaldb;Database=CinemaReservations;Trusted_Connection=True;MultipleActiveResultSets=true";
 
             ApplicationDbContext context = new ApplicationDbContext(connectionString);
 
             //Uncomment to run database seed
-            context.SeedDatabase(file);            
+            //context.SeedDatabase(file);            
         }
     }
 }
