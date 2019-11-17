a90966fdc18f7ae08f829b1adeb61e168e15342b
Armin Odobasic <armindbsc@gmail.com>
2019-11-17 19:43:44 +0100

    Changed relation between Reservation and Invoice

--- Cinema.Domain/Entities/Pricing.cs
+++ Cinema.Domain/Entities/Pricing.cs
@@ -1,7 +1,14 @@
-﻿namespace Cinema.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace Cinema.Domain.Entities
 {
     public class Pricing : BaseType
     {
+        public Pricing()
+        {
+            Invoices = new List<Invoice>();
+        }
         public decimal Price { get; set; }
+        public virtual IList<Invoice> Invoices { get; set; }
     }
 }
