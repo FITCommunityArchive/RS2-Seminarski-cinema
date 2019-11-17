356e183251b73b4f190924f9bfbe227c6af99afa
Armin Odobasic <armindbsc@gmail.com>
2019-11-17 16:48:02 +0100

    Added get and get by id tests for Halls

--- Cinema.Test/DALTests/HallsTest.cs
+++ Cinema.Test/DALTests/HallsTest.cs
@@ -13,19 +13,31 @@ namespace Cinema.Test.DALTests
     public class HallsTest : TestBase
     {
         [Test, Order(1)]
-        public void GetAllHalls()
+        public async Task GetAllHalls()
         {
+            //Arrange
             DbSet<Hall> halls = context.Halls;
-  
+                                             
+            //Act
+
+            //Assert
             //There are 2 halls in the test database
-            Assert.AreEqual(2, halls.CountAsync());
+             Assert.AreEqual(2, await halls.CountAsync());
+
+            /*
+            IndexModel indexModel = new IndexModel(context);
+
+            await indexModel.OnGetAsync();
+            var result = indexModel.Hall;
+
+            Assert.AreEqual(2, result.Count);*/
         }
 
         [Test, Order(1)]
         [TestCase(2)]
         public async Task GetHallById(int id)
         {
-            //Try to get Hall with id nr. 2 through IndexModel
+            //Try to get Hall with id nr. 2
             Hall hall = await context.Halls.FirstOrDefaultAsync(x => x.Id == id);       
 
             Assert.AreEqual("Movie Hall 2", hall.Name);            
