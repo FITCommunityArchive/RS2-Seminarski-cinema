2edc98e2d99f43dec5ca9eb13f5e1c523ba09049
Boris Huseincehajic <boris.huseincehajic@edu.fit.ba>
2019-12-07 19:31:11 +0100

    Nowshowing pagination,filter and details

--- Cinema.Web/wwwroot/js/site.js
+++ Cinema.Web/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+jQuery(document).ready(function () {
+
+    var options = {
+        format: 'dd.mm.yyyy',
+        todayHighlight: true,
+        autoclose: true,
+    };
+    jQuery("#ScreeningDate").datepicker(options);
+
+    $.date = function (dateObject) {
+        var d = new Date(dateObject);
+        var day = d.getDate();
+        var month = d.getMonth() + 1;
+        var year = d.getFullYear();
+        if (day < 10) {
+            day = "0" + day;
+        }
+        if (month < 10) {
+            month = "0" + month;
+        }
+        var date = day + "/" + month + "/" + year;
+
+        return date;
+    };
+
+    //jQuery("#MovieDatePicker .btn").click(function () {
+    //    var selectedDate = new Date(jQuery("#ScreeningDate").val());
+
+    //    //jQuery("#date").val(jQuery.date(selectedDate));
+    //    //console.log(jQuery.date(selectedDate));
+    //    jQuery("form").submit();
+    //})
+});
