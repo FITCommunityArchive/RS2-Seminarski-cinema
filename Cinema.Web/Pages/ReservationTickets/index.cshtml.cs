14f53ea04219efddcc13716109cd93fa48cb4c1b
Armin Odobasic <armindbsc@gmail.com>
2020-01-06 20:26:03 +0100

    Added SeatingService to ReservationTickets IndexModel

--- Cinema.Web/Pages/ReservationTickets/index.cshtml.cs
+++ Cinema.Web/Pages/ReservationTickets/index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Cinema.Domain.Entities;
+using Cinema.DTO.SpecificModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,19 +14,17 @@ namespace Cinema.Web.Pages.ReservationTickets
     {
 
         private readonly Cinema.Web.Data.ApplicationDbContext _context;
+        private SeatingService _seatingService;
 
         public IndexModel(Cinema.Web.Data.ApplicationDbContext context)
         {
             _context = context;
+            _seatingService = new SeatingService(context);           
         }
 
         public Hall CurrentHall { get; set; }
-        public DateTime ScreeningDate { get; set; }
         public Screening CurrentScreening { get; set; }
-        public List<Reservation> ScreeningReservations { get; set; }
-        public List<SeatReservation> SeatReservationsList { get; set; }
-
-        public List<int> ReservedSeatNumbers { get; set; }
+        public List<SeatingModel> ScreeningSeats { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id, long date)
         {
@@ -35,17 +34,16 @@ namespace Cinema.Web.Pages.ReservationTickets
                 return NotFound();
             }
 
-            ScreeningDate = new DateTime(date);
+            var screeningDate = new DateTime(date);
             CurrentHall = await _context.Halls
                 .FirstOrDefaultAsync(x => x.Id == id);
 
-            //CurrentScreening = CurrentHall.Screenings.FirstOrDefault(x => x.DateAndTime == ScreeningDate);
+            CurrentScreening = CurrentHall.Screenings.FirstOrDefault(x => x.DateAndTime == screeningDate);
 
-            CurrentScreening = _context.Halls.FirstOrDefault(x => x.Id == id).Screenings
-                .FirstOrDefault(x => x.DateAndTime == ScreeningDate);
+            //CurrentScreening = _context.Halls.FirstOrDefault(x => x.Id == id).Screenings
+                //.FirstOrDefault(x => x.DateAndTime == ScreeningDate);
 
-            //ReservedSeatNumbers = GetSeatNumbers();
-
+            ScreeningSeats = _seatingService.GetScreeningSeating(CurrentScreening);
 
             return Page();
         }
