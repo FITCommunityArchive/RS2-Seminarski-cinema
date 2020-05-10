70723920ad31173cb1fa654d9caac03ae52aa387
Boris Huseincehajic <boris.huseincehajic@edu.fit.ba>
2020-05-10 17:07:30 +0200

    Fix contact form validation messages and logic | fix date check on nowshowing

---
+++ Cinema.DTO/ViewModels/Home/HomeContactVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cinema.DTO.ViewModels.Home
+{
+    public class HomeContactVM
+    {
+        [Required, StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
