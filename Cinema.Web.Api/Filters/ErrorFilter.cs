99ea8600f0ce246e8918592c7b41e805e3a99429
Armin Odobašić <armin.odobasic@edu.fit.ba>
2020-07-07 15:26:17 +0200

    Added ErrorFilter

---
+++ Cinema.Web.Api/Filters/ErrorFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace eProdaja.WebAPI.Filters
+{
+    public class ErrorFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            context.ModelState.AddModelError("ERROR", "Internal Server Error");
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            context.Result = new JsonResult(context.ModelState);
+        }
+
+    }
+}
