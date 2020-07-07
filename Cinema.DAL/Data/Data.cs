0c8f7381b15dfd9e990efd303f12af00b5ec7426
Armin Odobašić <armin.odobasic@edu.fit.ba>
2020-07-07 16:33:40 +0200

    Added initial data in Data

--- Cinema.DAL/Data/Data.cs
+++ Cinema.DAL/Data/Data.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Cinema.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime;
 using System;
 
 namespace Cinema.Dal.Data
@@ -18,8 +20,27 @@ namespace Cinema.Dal.Data
         }
 
         public static void Seed(ModelBuilder modelBuilder)
-        {
-           
+        {/*
+            modelBuilder.Entity<Hall>().HasData(
+                new Hall
+                {
+                    Id = -1,
+                    CreatedAt = new DateTime(2020, 7, 7),
+                    IsDeleted = false,
+                    Name = "Movie Hall 1",
+                    NumberOfColumns = 16,
+                    NumberOfRows = 15                
+                },
+                new Hall
+                {
+                    Id = -2,
+                    CreatedAt = new DateTime(2020, 7, 7),
+                    IsDeleted = false,
+                    Name = "Movie Hall 2",
+                    NumberOfColumns = 16,
+                    NumberOfRows = 15
+                }
+            );*/
         }
     }
 }
