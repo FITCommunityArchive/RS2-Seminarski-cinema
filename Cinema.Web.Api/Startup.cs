7124b1351e178baf588074757726d0115432bad4
Armin Odobašić <armin.odobasic@edu.fit.ba>
2020-07-07 15:44:33 +0200

    Code cleanup

--- Cinema.Web.Api/Startup.cs
+++ Cinema.Web.Api/Startup.cs
@@ -1,22 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Cinema.Dal.Data;
 using Cinema.Web.Api.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Cinema.Web.API
 {
@@ -40,7 +31,7 @@ namespace Cinema.Web.API
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cinema API", Version = "v1" });               
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cinema API", Version = "v1" });
             });
 
             string connection = Configuration.GetConnectionString("DefaultConnection");
