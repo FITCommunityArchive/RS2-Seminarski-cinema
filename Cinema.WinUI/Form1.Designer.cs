0867cbdce07d5a42a1ed14f0ed2534dd182ff9f6
Boris Huseincehajic <boris.huseincehajic@edu.fit.ba>
2020-07-20 21:25:14 +0200

    Add WinUI, Index Form with the panels included and childForm examples, closing and openings

---
+++ Cinema.WinUI/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Cinema.WinUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panelDashboardSubmenu = new System.Windows.Forms.Panel();
+            this.buttonDashaboardSubmenu2 = new System.Windows.Forms.Button();
+            this.buttonDashaboardSubmenu1 = new System.Windows.Forms.Button();
+            this.buttonDashboard = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelTopBar = new System.Windows.Forms.Panel();
+            this.panelBottomBar = new System.Windows.Forms.Panel();
+            this.panelChildForm = new System.Windows.Forms.Panel();
+            this.buttonLogOut = new System.Windows.Forms.Button();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelSideMenu.SuspendLayout();
+            this.panelDashboardSubmenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTopBar.SuspendLayout();
+            this.panelBottomBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(88)))), ((int)(((byte)(209)))));
+            this.panelSideMenu.Controls.Add(this.button5);
+            this.panelSideMenu.Controls.Add(this.button4);
+            this.panelSideMenu.Controls.Add(this.panelDashboardSubmenu);
+            this.panelSideMenu.Controls.Add(this.buttonDashboard);
+            this.panelSideMenu.Controls.Add(this.panelLogo);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(250, 591);
+            this.panelSideMenu.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(0, 316);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(250, 45);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Screenings";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(0, 271);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(250, 45);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Movies";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panelDashboardSubmenu
+            // 
+            this.panelDashboardSubmenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(53)))), ((int)(((byte)(121)))));
+            this.panelDashboardSubmenu.Controls.Add(this.buttonDashaboardSubmenu2);
+            this.panelDashboardSubmenu.Controls.Add(this.buttonDashaboardSubmenu1);
+            this.panelDashboardSubmenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDashboardSubmenu.Location = new System.Drawing.Point(0, 190);
+            this.panelDashboardSubmenu.Name = "panelDashboardSubmenu";
+            this.panelDashboardSubmenu.Size = new System.Drawing.Size(250, 81);
+            this.panelDashboardSubmenu.TabIndex = 2;
+            // 
+            // buttonDashaboardSubmenu2
+            // 
+            this.buttonDashaboardSubmenu2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDashaboardSubmenu2.FlatAppearance.BorderSize = 0;
+            this.buttonDashaboardSubmenu2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.buttonDashaboardSubmenu2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.buttonDashaboardSubmenu2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDashaboardSubmenu2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDashaboardSubmenu2.Location = new System.Drawing.Point(0, 40);
+            this.buttonDashaboardSubmenu2.Name = "buttonDashaboardSubmenu2";
+            this.buttonDashaboardSubmenu2.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.buttonDashaboardSubmenu2.Size = new System.Drawing.Size(250, 40);
+            this.buttonDashaboardSubmenu2.TabIndex = 1;
+            this.buttonDashaboardSubmenu2.Text = "Submenu 2";
+            this.buttonDashaboardSubmenu2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonDashaboardSubmenu2.UseVisualStyleBackColor = true;
+            this.buttonDashaboardSubmenu2.Click += new System.EventHandler(this.buttonDashaboardSubmenu2_Click);
+            // 
+            // buttonDashaboardSubmenu1
+            // 
+            this.buttonDashaboardSubmenu1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDashaboardSubmenu1.FlatAppearance.BorderSize = 0;
+            this.buttonDashaboardSubmenu1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.buttonDashaboardSubmenu1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.buttonDashaboardSubmenu1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDashaboardSubmenu1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDashaboardSubmenu1.Location = new System.Drawing.Point(0, 0);
+            this.buttonDashaboardSubmenu1.Name = "buttonDashaboardSubmenu1";
+            this.buttonDashaboardSubmenu1.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.buttonDashaboardSubmenu1.Size = new System.Drawing.Size(250, 40);
+            this.buttonDashaboardSubmenu1.TabIndex = 0;
+            this.buttonDashaboardSubmenu1.Text = "Submenu 1";
+            this.buttonDashaboardSubmenu1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonDashaboardSubmenu1.UseVisualStyleBackColor = true;
+            this.buttonDashaboardSubmenu1.Click += new System.EventHandler(this.buttonDashaboardSubmenu1_Click);
+            // 
+            // buttonDashboard
+            // 
+            this.buttonDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDashboard.FlatAppearance.BorderSize = 0;
+            this.buttonDashboard.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.buttonDashboard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(42)))), ((int)(((byte)(97)))));
+            this.buttonDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDashboard.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDashboard.Location = new System.Drawing.Point(0, 145);
+            this.buttonDashboard.Name = "buttonDashboard";
+            this.buttonDashboard.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.buttonDashboard.Size = new System.Drawing.Size(250, 45);
+            this.buttonDashboard.TabIndex = 1;
+            this.buttonDashboard.Text = "Dashboard";
+            this.buttonDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonDashboard.UseVisualStyleBackColor = true;
+            this.buttonDashboard.Click += new System.EventHandler(this.buttonDashboard_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.button1);
+            this.panelLogo.Controls.Add(this.label3);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(250, 145);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // panelTopBar
+            // 
+            this.panelTopBar.BackColor = System.Drawing.Color.White;
+            this.panelTopBar.Controls.Add(this.label1);
+            this.panelTopBar.Controls.Add(this.buttonLogIn);
+            this.panelTopBar.Controls.Add(this.buttonLogOut);
+            this.panelTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopBar.Location = new System.Drawing.Point(250, 0);
+            this.panelTopBar.Name = "panelTopBar";
+            this.panelTopBar.Size = new System.Drawing.Size(684, 50);
+            this.panelTopBar.TabIndex = 1;
+            // 
+            // panelBottomBar
+            // 
+            this.panelBottomBar.BackColor = System.Drawing.Color.White;
+            this.panelBottomBar.Controls.Add(this.label2);
+            this.panelBottomBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottomBar.Location = new System.Drawing.Point(250, 559);
+            this.panelBottomBar.Name = "panelBottomBar";
+            this.panelBottomBar.Size = new System.Drawing.Size(684, 32);
+            this.panelBottomBar.TabIndex = 2;
+            // 
+            // panelChildForm
+            // 
+            this.panelChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChildForm.Location = new System.Drawing.Point(250, 50);
+            this.panelChildForm.Name = "panelChildForm";
+            this.panelChildForm.Size = new System.Drawing.Size(684, 509);
+            this.panelChildForm.TabIndex = 3;
+            // 
+            // buttonLogOut
+            // 
+            this.buttonLogOut.Location = new System.Drawing.Point(597, 12);
+            this.buttonLogOut.Name = "buttonLogOut";
+            this.buttonLogOut.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogOut.TabIndex = 0;
+            this.buttonLogOut.Text = "Log Out";
+            this.buttonLogOut.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Location = new System.Drawing.Point(516, 12);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogIn.TabIndex = 1;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome user";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Copyright eCinema © 2014-2020";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(9, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Boris Huseincehajic";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 591);
+            this.Controls.Add(this.panelChildForm);
+            this.Controls.Add(this.panelBottomBar);
+            this.Controls.Add(this.panelTopBar);
+            this.Controls.Add(this.panelSideMenu);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelSideMenu.ResumeLayout(false);
+            this.panelDashboardSubmenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTopBar.ResumeLayout(false);
+            this.panelTopBar.PerformLayout();
+            this.panelBottomBar.ResumeLayout(false);
+            this.panelBottomBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Panel panelDashboardSubmenu;
+        private System.Windows.Forms.Button buttonDashaboardSubmenu2;
+        private System.Windows.Forms.Button buttonDashaboardSubmenu1;
+        private System.Windows.Forms.Button buttonDashboard;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panelTopBar;
+        private System.Windows.Forms.Panel panelBottomBar;
+        private System.Windows.Forms.Panel panelChildForm;
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.Button buttonLogOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Cinema.WinUI/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinema.WinUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            customizeDesign();
+        }
+
+
+        private void customizeDesign()
+        {
+            panelDashboardSubmenu.Visible = false;
+        }
+
+        private void hideSubmenu()
+        {
+            if(panelDashboardSubmenu.Visible == true)
+            {
+                panelDashboardSubmenu.Visible = false;
+            }
+        }
+
+        private void ShowSubmenu(Panel submenu)
+        {
+            if(submenu.Visible == false)
+            {
+                hideSubmenu();
+                submenu.Visible = true;
+            } else
+            {
+                submenu.Visible = false;
+            }
+        }
+
+        private void buttonDashboard_Click(object sender, EventArgs e)
+        {
+            ShowSubmenu(panelDashboardSubmenu);
+            openChildForm(new FormDashboard());
+        }
+
+        private void buttonDashaboardSubmenu1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDashaboardSubmenu2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if(activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+    }
+}
