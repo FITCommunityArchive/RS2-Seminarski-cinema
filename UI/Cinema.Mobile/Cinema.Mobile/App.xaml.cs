f645577e8704f1319b3f7917623e63c45974e7ed
Boris Huseincehajic <boris.huseincehajic@edu.fit.ba>
2020-08-24 21:19:36 +0200

    Add login and register xaml pages

--- UI/Cinema.Mobile/Cinema.Mobile/App.xaml.cs
+++ UI/Cinema.Mobile/Cinema.Mobile/App.xaml.cs
@@ -14,7 +14,7 @@ namespace Cinema.Mobile
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Login());
         }
 
         protected override void OnStart()
