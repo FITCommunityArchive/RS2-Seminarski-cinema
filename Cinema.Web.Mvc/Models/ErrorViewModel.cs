e6a8a76767595c52f58c0e6f749371a96065aa39
Armin Odobašić <armin.odobasic@edu.fit.ba>
2020-09-13 14:14:04 +0200

    Removed unused project folders

--- Cinema.Web.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cinema.Web.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
